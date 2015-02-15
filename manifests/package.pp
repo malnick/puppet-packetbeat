5e35e4ce317efee73797162c68f3a2347c28d6e1
malnick <malnick@gmail..com>
2015-02-15 08:49:41 -0800

    package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,7 +3,6 @@ class packetbeat::package {
     $package = $::osfamiy ? {
         'Debian' => '/usr/bin/curl -L -o /tmp/packetbeat https://github.com/packetbeat/packetbeat/releases/download/v0.4.3/packetbeat_0.4.3-1_amd64.deb',
         'RedHat' => '/usr/bin/curl -L -o /tmp/packetbeat https://github.com/packetbeat/packetbeat/releases/download/v0.4.3/packetbeat-0.4.3-1.el6.x86_64.rpm',
-        default  => fail("Sorry, we only have RedHat and Debian packages available")
     }
 
     exec { 'download_packetbeat':
