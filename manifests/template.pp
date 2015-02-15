5b70ef86004d81e80389c8752bf5056c85250ece
malnick <malnick@gmail..com>
2015-02-15 09:19:40 -0800

    temp

---
+++ manifests/template.pp
@@ -0,0 +1,14 @@
+class packetbeat::template {
+
+    file { "packetbeat.template.json":
+        ensure => present,
+        source => 'puppet:///modules/packetbeat/packetbeat.template.json',
+        path   => '/tmp/packetbeat.template.json',
+    }
+
+    exec { "put_dashboards":
+        command => '/usr/bin/curl -XPUT "http://localhost:9200/_template/packetbeat" -d@/tmp/packetbeat.template.json',
+        unless  => '/usr/bin/curl -XGET "http://localhost:9200/_template/packetbeat?pretty"',
+        require => File['packetbeat.template.json'],
+    }
+}
