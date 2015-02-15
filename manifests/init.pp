bf16ffc90a245bc7c4da600fe74013d119186d55
malnick <malnick@gmail..com>
2015-02-15 07:34:04 -0800

    package

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,17 +21,19 @@
 # GPLv2
 #
 class packetbeat (
-  $host = $packetbeat::params::host,
-  $port = $packetbeat::params::port,
-  $interfaces = $packetbeat::params::interfaces,
-  $disable_procs = $packetbeat::params::disable_procs,
-  $agentname = $packetbeat::params::agentname ,
-  $uid = $packetbeat::params::uid,
-  $gid = $packetbeat::params::gid,
+  $host             = $packetbeat::params::host,
+  $port             = $packetbeat::params::port,
+  $interfaces       = $packetbeat::params::interfaces,
+  $disable_procs    = $packetbeat::params::disable_procs,
+  $agentname        = $packetbeat::params::agentname ,
+  $uid              = $packetbeat::params::uid,
+  $gid              = $packetbeat::params::gid,
 ) inherits packetbeat::params {
 
-  include packetbeat::package
-  include packetbeat::config
-  include packetbeat::service
+  class { ::packetbeat::package:}
+  ->
+  class { ::packetbeat::config: }
+  ->
+  class { ::packetbeat::service: }
 
 }
--- manifests/package.pp
+++ manifests/package.pp
@@ -1,8 +1,21 @@
-class packetbeat::package ($version = 'latest'){
-  package { 'packetbeat':
-    ensure => $version,
-  }
+class packetbeat::package {
+    
+    $package = $::osfamiy ? {
+        'Debian' => '/usr/bin/curl -L -o /tmp/packetbeat https://github.com/packetbeat/packetbeat/releases/download/v0.4.3/packetbeat_0.4.3-1_amd64.deb',
+        'RedHat' => '/usr/bin/curl -L -o /tmp/packetbeat https://github.com/packetbeat/packetbeat/releases/download/v0.4.3/packetbeat-0.4.3-1.el6.x86_64.rpm',
+        default  => fail("Sorry, we only have RedHat and Debian packages available")
+    }
 
+    exec { 'download_packetbeat':
+        command => $package,
+        creates => '/tmp/packetbeat',
+    }
+
+    package { '/tmp/packetbeat':
+        ensure   => installed,
+        provider => 'dpkg',
+        require  => Exec['download_packetbeat'],
+    }
 }
 
 
