979ad3a1b766d59e0e1f59e233f43583bf874216
malnick <malnick@gmail..com>
2015-02-15 08:55:11 -0800

    package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,10 +1,10 @@
 class packetbeat::package {
    
     if $::osfamily == 'Debian' {
-        $package = '/usr/bin/curl -L -o /tmp/packetbeat https://github.com/packetbeat/packetbeat/releases/download/v0.4.3/packetbeat_0.4.3-1_amd64.deb',
+        $package = '/usr/bin/curl -L -o /tmp/packetbeat https://github.com/packetbeat/packetbeat/releases/download/v0.4.3/packetbeat_0.4.3-1_amd64.deb'
     }
     elsif $::osfamily == 'RedHat' {
-        $package = '/usr/bin/curl -L -o /tmp/packetbeat https://github.com/packetbeat/packetbeat/releases/download/v0.4.3/packetbeat-0.4.3-1.el6.x86_64.rpm',
+        $package = '/usr/bin/curl -L -o /tmp/packetbeat https://github.com/packetbeat/packetbeat/releases/download/v0.4.3/packetbeat-0.4.3-1.el6.x86_64.rpm'
     }
 
     exec { 'download_packetbeat':
