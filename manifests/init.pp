6bdb37192089153923ca5f5a33bbbe882e2f64fb
malnick <malnick@gmail..com>
2015-02-15 16:49:42 -0800

    templates

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,23 @@
 class packetbeat (
   
-  $aggregator       = $packetbeat::params::host,
-  $port             = $packetbeat::params::port,
+  $aggregator       = $packetbeat::params::aggregator,
+  $es_port          = $packetbeat::params::es_port,
   $interfaces       = $packetbeat::params::interfaces,
   $disable_procs    = $packetbeat::params::disable_procs,
   $agentname        = $packetbeat::params::agentname ,
   $uid              = $packetbeat::params::uid,
   $gid              = $packetbeat::params::gid,
+  $es_enabled       = $packetbeat::params::es_enabled,
+  $redis_enabled    = $packetbeat::params::redis_enabled,
+  # Which procs to monitor
+  $mysqld_mon       = $packetbeat::params::mysqld_mon,
+  $nginx_mon        = $packetbeat::params::nginx_mon,
+  $app_procs        = $packetbeat::params::app_procs,
+  # Protocols, each has an array of ports 
+  $http_protocols   = $packetbeat::params::http_protocols,
+  $mysql_protocols  = $packetbeat::params::mysql_protocols,
+  $redis_protocols  = $packetbeat::params::redis_protocols,
+  $pgsql_protocols  = $packetbeat::params::pgsql_protocols,
 
 ) inherits packetbeat::params {
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,23 @@
 class packetbeat::params(
   $aggregator       = 'localhost',
-  $port             = '9200',
+  $redis_host       = 'localhost',
+  $es_port          = '9200',
   $interfaces       = 'any',
-  $disable_procs    = true,
+  $disable_procs    = false,
   $agentname        = $::fqdn,
   $uid              = '501',
   $gid              = '501',
   $ensure           = 'running',
   $enable           = true,
+  $es_enabled       = true,
+  $redis_enabled    = false,
+  $mysqld_mon       = false,
+  $nginx_mon        = false,
+  $app_procs        = false,
+  $http_protocols   = [80, 8080, 8000, 5000, 8002],
+  $mysql_protocols  = undef, #[3306],
+  $redis_protocols  = undef, #[6379],
+  $pgsql_protocols  = undef, #[5432],
 ){
 
 
